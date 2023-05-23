ff89df32223c7ecbceb674e3936aef4a7464f1a9
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-23 16:13:52 -0400

    corrected syntax for handleSearch

--- src/screens/LandingPage.tsx
+++ src/screens/LandingPage.tsx
@@ -1,25 +1,33 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import React from 'react';
 import Banner from '../components/Banner';
 import SearchField from '../components/SearchField';
 import {useState} from 'react';
+import Results from '../components/Results';
 
-// const apiUrl: string = `http://www.omdbapi.com/?t=${}apikey=615a6ca8&t=`;
-
+export type MovieResultType = {
+  Title: string;
+  Poster: string;
+  Rated: string;
+  Plot: string;
+};
 const LandingPage = () => {
-  const [movieResults, setMovieResults] = useState<string>('');
+  const [movieResult, setMovieResult] = useState<MovieResultType | null>(null);
 
-  const handleSearch = async (searchString: string) => {
+  const handleSearch = async (searchString: string): Promise<void> => {
     const result = await fetch(
       `https://www.omdbapi.com/?t=${searchString}&apikey=2c0bcb9f`,
     ).then(data => data.json());
-    setMovieResults(result);
+    setMovieResult(result);
   };
 
   return (
     <SafeAreaView className="min-h-screen min-w-screen">
       <Banner />
       <SearchField onSubmit={handleSearch} />
+      <View className="flex-row justify-center">
+        {movieResult?.Title && <Results movie={movieResult} />}
+      </View>
     </SafeAreaView>
   );
 };
