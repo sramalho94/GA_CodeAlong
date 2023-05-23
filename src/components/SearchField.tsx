f8a9fd1e8b7ad1bb53789d464553cf79cdba8f06
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-23 12:13:31 -0400

    built out search field

---
+++ src/components/SearchField.tsx
@@ -0,0 +1,28 @@
+import {View, TextInput, Pressable, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {useState} from 'react';
+
+type Props = {onSubmit: Function};
+
+const SearchField = ({onSubmit}: Props) => {
+  const [searchString, setSearchString] = useState<string>('');
+
+  const handleSearch = () => {
+    onSubmit(searchString);
+  };
+  return (
+    <View className="flex-row mr-1">
+      <TextInput
+        className="border rounded-md border-gray border-s p-2 m-2 flex-1"
+        onChangeText={setSearchString}
+      />
+      <TouchableOpacity
+        className="bg-green-500 my-2 p-1 rounded-md border border-s border-gray-300 justify-center"
+        onPress={handleSearch}>
+        <Text className="text-white">Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchField;
